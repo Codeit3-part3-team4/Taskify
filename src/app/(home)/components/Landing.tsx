6e30e6cbe70f3afe28b4fbaec7862ae92fd899f3
ohddang <ohddang509@gmail.com>
2024-03-09 15:20:58 +0900

    refactor: 스타일 세부 컴포넌트로 분리

--- src/app/(home)/components/Landing.tsx
+++ src/app/(home)/components/Landing.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 
 export default function Landing() {
-  return (
-    <div className="flex flex-col justify-start items-center min-h-screen text-white">
-      <section className="flex flex-col items-center mb-20 vertical-middle text-center align-middle">
+  const Title = () => {
+    return (
+      <div className="flex flex-col items-center vertical-middle text-center align-middle">
         <div className="mb-10">
-          <Image src="/images/add.svg" alt="thumb" width="287" height="168" />
+          <div className="w-[287px] h-[168px] rounded-lg overflow-hidden bg-white">
+            <div className="w-full h-full"/>
+          </div>
         </div>
         <div className="flex flex-col items-center text-4xl letter leading-10 gap-1">
           <strong className="mb-3">새로운 일정 관리</strong>
@@ -15,54 +17,76 @@ export default function Landing() {
         <div className="flex justify-center items-center rounded-lg w-60 h-11 bg-violet-500/90">
           <button className="w-full h-full">로그인하기</button>
         </div>
-      </section>
-      <section className="flex flex-col justify-between w-[340px] h-[686px] rounded-lg overflow-hidden mb-16 bg-gray-300/10">
+      </div>
+    )
+  }
+
+  const Thumb = ({width, height}: {width: number, height: number}) => {
+    return (
+      <div className={`w-[${width}px] h-[${height}px] rounded-lg overflow-hidden bg-white`}>
+        <div className="w-full h-full" />
+      </div>
+    )
+  }
+
+  const MainThumb = ({ title, desc, desc2, children }: { title: string, desc: string, desc2: string, children: React.ReactNode }) => {
+    return (
+      <div className="flex flex-col justify-between w-[340px] h-[686px] rounded-lg overflow-hidden bg-gray-300/10">
         <div className="flex flex-col items-center mt-16">
-          <span className="text-lg text-gray-400 mb-20">Point 1</span>
-          <strong className="text-3xl text-center">일의 우선순위를<br/>관리하세요</strong>
+          <span className="text-lg text-gray-400 mb-20">{title}</span>
+          <strong className="text-3xl text-center">{desc}<br/>{desc2}</strong>
         </div>
-        <div className="flex flex-row justify-end">
-          <Image src="/images/add.svg" alt="thumb" width="287" height="168" />
+        {children}
+      </div>
+    );
+  }
+
+  const MiniThumb = ({ title, desc}: { title: string, desc: string }) => {
+    return (
+      <div className="w-[340px] h-[340px] rounded-lg overflow-hidden">
+        <div className="flex justify-center items-center w-full h-2/3 bg-gray-300/40">
+          <div className="w-64 h-48 rounded-lg overflow-hidden bg-white"></div>
         </div>
+        <div className="flex flex-col justify-between w-full h-1/3 bg-gray-500/40 px-5 py-7">
+          <strong className="text-base">{title}</strong>
+          <span className="text-sm">{desc}</span>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col justify-start items-center min-h-screen text-white">
+      <section className="mb-20">
+        <Title />
       </section>
-      <section className="flex flex-col justify-between w-[340px] h-[686px] rounded-lg overflow-hidden mb-24 bg-gray-300/10">
-        <div className="flex flex-col items-center mt-16">
-          <span className="text-lg text-gray-400 mb-20">Point 2</span>
-          <strong className="text-3xl text-center">해야 할 일을<br/>등록하세요</strong>
-        </div>
-        <div className="flex flex-row justify-end">
-          <Image src="/images/add.svg" alt="thumb" width="287" height="168" />
-        </div>
+      <section className="mb-16">
+        <MainThumb title="Point 1" desc="일의 우선순위를" desc2="관리하세요">
+          <div className="flex flex-row justify-end">
+            <Thumb width={296} height={248} />
+          </div>
+        </MainThumb>
+      </section>
+      <section className="mb-24">
+        <MainThumb title="Point 2" desc="해야 할 일을" desc2="등록하세요">
+          <div className="flex flex-row justify-center">
+            <Thumb width={217} height={250} />
+          </div>
+        </MainThumb>
       </section>
       <section className="mb-32">
-        <div className="text-xl text-center mb-10">생산성을 높이는 다양한 설정 ⚡ </div>
+        <div className="mb-10 text-center">
+          <span className="text-xl">생산성을 높이는 다양한 설정 ⚡ </span>
+        </div>
         <div>
-          <div className="mb-10 w-[340px] h-[340px] rounded-lg overflow-hidden ">
-            <div className="flex justify-center items-center w-full h-2/3 bg-gray-300/40">
-              <div className="w-64 h-24 rounded-lg overflow-hidden bg-white"></div>
-            </div>
-            <div className="flex flex-col justify-between w-full h-1/3 bg-gray-500/40 px-5 py-7">
-              <strong className="text-base">대시보드 설정</strong>
-              <div className="text-sm">대시보드 사진과 이름을 변경할 수 있어요.</div>
-            </div>
+          <div className="mb-10">
+            <MiniThumb title="대시보드" desc="대시보드 사진과 이름을 변경할 수 있어요." />
           </div>
-          <div className="mb-10 w-[340px] h-[340px] rounded-lg overflow-hidden ">
-            <div className="flex justify-center items-center w-full h-2/3 bg-gray-300/40">
-              <div className="w-64 h-48 rounded-lg overflow-hidden bg-white"></div>
-            </div>
-            <div className="flex flex-col justify-between w-full h-1/3 bg-gray-500/40 px-5 py-7">
-              <strong className="text-base">초대</strong>
-              <div className="text-sm">새로운 팀원을 초대할 수 있어요.</div>
-            </div>
+          <div className="mb-10">
+            <MiniThumb title="초대" desc="새로운 팀원을 초대할 수 있어요." />
           </div>
-          <div className="w-[340px] h-[340px] rounded-lg overflow-hidden ">
-            <div className="flex justify-center items-center w-full h-2/3 bg-gray-300/40">
-              <div className="w-64 h-40 rounded-lg overflow-hidden bg-white"></div>
-            </div>
-            <div className="flex flex-col justify-between w-full h-1/3 bg-gray-500/40 px-5 py-7">
-              <strong className="text-base">구성원</strong>
-              <div className="text-sm">구성원을 초대하고 내보낼 수 있어요.</div>
-            </div>
+          <div>
+            <MiniThumb title="구성원" desc="구성원을 초대하고 내보낼 수 있어요."/>
           </div>
         </div>
       </section>
