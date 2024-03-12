c89497a46acc3472af9b3173a13cda3e5999db22
unknown <kimksy27@gmail.com>
2024-03-12 20:31:25 +0900

    feat: 계정관리 페이지 뼈대 구현

--- src/components/mypage/myaccounts.tsx
+++ src/components/mypage/myaccounts.tsx
@@ -7,15 +7,58 @@ const MyAccounts: React.FC = () => {
 
   return (
     <div>
-      <h2>프로필</h2>
-      {userInfo && (
-        <div>
-          <div>이메일</div>
-          <input value={userInfo.email} />
-          <div>닉네임</div>
-          <input value={userInfo.nickname} />
-        </div>
-      )}
+      <div>
+        <h2>프로필</h2>
+        {userInfo && (
+          <div>
+            <div>
+              <img src={userInfo.profileImageUrl} alt="프로필 사진" />
+              <div>
+                <div>이메일</div>
+                <input value={userInfo.email} />
+                <form>
+                  <div>닉네임</div>
+                  <input value={userInfo.nickname} />
+                </form>
+              </div>
+            </div>
+            <button>저장</button>
+          </div>
+        )}
+      </div>
+      <div>
+        <h2>비밀번호 변경</h2>
+        <form>
+          <label htmlFor="password">현재 비밀번호</label>
+          <div>
+            <input
+              type="password"
+              id="password"
+              value=""
+              placeholder="현재 비밀번호 입력"
+            />
+          </div>
+          <label htmlFor="newPassword">새 비밀번호</label>
+          <div>
+            <input
+              type="password"
+              id="newPassword"
+              value=""
+              placeholder="새 비밀번호 입력"
+            />
+          </div>
+          <label htmlFor="newPwCheck">새 비밀번호 확인</label>
+          <div>
+            <input
+              type="password"
+              id="newPwCheck"
+              value=""
+              placeholder="새 비밀번호 입력"
+            />
+          </div>
+        </form>
+        <button>변경</button>
+      </div>
     </div>
   );
 };
