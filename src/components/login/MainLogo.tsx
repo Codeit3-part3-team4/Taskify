abdaac83266828a214dde72cf7f6951c6f1695b1
unknown <kimksy27@gmail.com>
2024-03-18 00:48:30 +0900

    Feat : 로그인 페이지 기능 구현

--- src/components/login/MainLogo.tsx
+++ src/components/login/MainLogo.tsx
@@ -5,12 +5,7 @@ export default function MainLogo({ title }) {
   return (
     <div>
       <Link href={'/'}>
-        <Image
-          src="/images/taskify-logo-main.svg"
-          alt="메인 로고"
-          width={200}
-          height={279}
-        />
+        <Image src="/images/taskify-logo-main.svg" alt="메인 로고" width={200} height={279} />
       </Link>
       <div>{title}</div>
     </div>
