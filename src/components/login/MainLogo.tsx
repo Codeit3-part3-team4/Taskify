b91b0ba11d77fad43bb651de60f62b2ad3f9f434
unknown <kimksy27@gmail.com>
2024-03-19 16:30:44 +0900

    .

--- src/components/login/MainLogo.tsx
+++ src/components/login/MainLogo.tsx
@@ -3,11 +3,17 @@ import Link from 'next/link';
 
 export default function MainLogo({ title }) {
   return (
-    <div className="flex flex-col justify-center items-center mt-223 md:w-576">
+    <div className="flex flex-col justify-center items-center mt-20">
       <Link href={'/'}>
-        <Image src="/images/taskify-logo-main.svg" alt="메인 로고" width={200} height={279} />
+        <Image src="/images/taskify-logo-main.svg" alt="메인 로고" width={200} height={279} className="w-120 h-167.4" />
       </Link>
       <div className="font-semibold">{title}</div>
     </div>
   );
 }
+
+/*
+w md:w lg
+
+
+ */
