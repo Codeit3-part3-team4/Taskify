c52b141ad78ee23bb82c329fb5a79d22716efd5f
2zzzyoung <mzooyoungn@gmail.com>
2024-03-20 17:35:08 +0900

    이미지 없을때 안보이게 수정

--- src/components/column/Column.tsx
+++ src/components/column/Column.tsx
@@ -27,7 +27,7 @@ const Column = ({ columnId, columnTitle, dashboardId }) => {
 
   return (
     <>
-      <div className="w-80 md:w-594 lg:w-96 lg:h-1080  bg-gray-100 border border-slate-300" key={columnId}>
+      <div className="w-80 md:w-594 lg:w-96 h-1080 bg-gray-100 border border-slate-300" key={columnId}>
         <div className="mx-3">
           <div className="flex justify-between py-4">
             <div className="flex items-center gap-2">
@@ -39,7 +39,7 @@ const Column = ({ columnId, columnTitle, dashboardId }) => {
           </div>
           <div className="flex md: justify-center">
             <button
-              className="flex justify-center items-center w-72 md:w-537 lg:w-80 h-8 md:h-10 rounded-md bg-white mb-5 border-2 border-slate-100 hover:border-purple-760DDE"
+              className="flex justify-center items-center w-72 md:w-537 lg:w-80 h-8 md:h-10 rounded-md bg-white mb-5 border-2 border-slate-100 hover:border-purple-760DDE transition duration-500"
               onClick={openModal}
             >
               <TodoForm dashboardId={dashboardId} columnId={columnId} />
@@ -58,23 +58,3 @@ const Column = ({ columnId, columnTitle, dashboardId }) => {
 };
 
 export default Column;
-
-// {cardList &&
-//   cardList?.cards
-//     // .filter(card => card.columnId === columnId)
-//     ?.map(card => (
-//       <div key={card.id}>
-//         <GetCard card={card} />
-//       </div>
-//     ))}
-// <div className="flex flex-row w-80 md:w-594 lg:w-96 h-full lg:h-1080 md:flex-col lg:flex-row lg:px-4 pt-4 bg-gray-100 border border-slate-300">
-//   <div className="w-80 md:w-594 lg:w-96">
-//     <button
-//       className="flex w-72 md:w-537 lg:w-80 lg:h-16 items-center m-auto bg-white border border-slate-300 justify-center gap-3 pt-3 lg:mt-10 pb-3 mb-3 rounded-lg cursor-pointer"
-//       // onClick={openModal}
-//     >
-//       <div className="text-base font-bold">새로운 컬럼 추가하기</div>
-//       <Image src="/images/add.svg" width="20" height="20" alt="컬럼 추가하기 버튼 아이콘" />
-//     </button>
-//   </div>
-// </div>
--- src/components/getCard/GetCard.tsx
+++ src/components/getCard/GetCard.tsx
@@ -15,13 +15,13 @@ const GetCard = ({ card }: { card: Card }) => {
     setIsOpen(false);
   };
 
-  console.log(card.imageUrl);
+  console.log(card);
 
   return (
     <>
-      <div className="flex flex-col gap-2.5" key={card.id}>
+      <div className="flex flex-col" key={card.id}>
         <div
-          className="flex flex-col md:flex-row m-auto w-72 md:w-537 lg:w-80 h-full bg-white items-center pt-3 pb-3 mb-3 rounded-lg border-2 border-slate-100 hover:border-purple-760DDE cursor-pointer bg-red-100"
+          className="flex flex-col md:flex-row m-auto w-72 md:w-537 lg:w-80 h-full bg-white items-center pt-3 pb-3 mb-3 rounded-lg border-2 border-slate-100 hover:border-purple-760DDE transition duration-500 cursor-pointer"
           onClick={clickOpenModal}
         >
           <Modal
@@ -33,18 +33,20 @@ const GetCard = ({ card }: { card: Card }) => {
           >
             <TodoCard cardId={card.id} isOpen={isOpen} />
           </Modal>
-          <div className="md:flex lg:flex-col md:m-auto md:gap-4">
-            {card.imageUrl && (
-              <div>
-                <img className="w-64 md:w-24 lg:w-72" src={card.imageUrl} alt="Card Image" />
+          <div className="md:flex lg:flex-col w-full">
+            {card.imageUrl !== 'https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/taskify/task_image/asdasd.png' && card.imageUrl !== null && (
+              <div className="w-284 md:w-28 lg:w-284 h-40 md:h-16 lg:h-32 overflow-hidden md:pl-4">
+                <img className="w-full h-full object-cover" src={card.imageUrl} alt="Card Image" />
               </div>
             )}
-            <div className="w-64 md:w-96 lg:w-72 pt-3 md:pt-0 md:flex-row ">
+            <div className="w-full pt-3 p-4 flex-col">
               <div>
                 <div className="text-sm font-medium leading-4 pb-1.5">{card.title}</div>
               </div>
-              <div className="md:flex md:items-center lg:flex-col">
-                <div className="bg-lime-100 text-lime-400 w-9 rounded-md py-1 px-1.5 pt-1.5 pb-1.5 text-xs text-center leading-3 md:mr-3.5">{card.tags}</div>
+              <div className="md:flex lg:flex-col">
+                <div className="lg:flex lg:justify-start">
+                  <div className="bg-lime-100 text-lime-400 w-9 rounded-md py-1 px-1.5 pt-1.5 pb-1.5 text-xs text-center leading-3 md:mr-3.5">{card.tags}</div>
+                </div>
                 <div className="flex w-64 md:w-full justify-between md:items-center">
                   <div className="flex w-full gap-1 items-center pt-1.5 md:pt-0">
                     <img src="/images/calendar-icon.svg" alt="캘린더 아이콘" />
@@ -59,7 +61,6 @@ const GetCard = ({ card }: { card: Card }) => {
           </div>
         </div>
       </div>
-      {/* </div> */}
     </>
   );
 };
