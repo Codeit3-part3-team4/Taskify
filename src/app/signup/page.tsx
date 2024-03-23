5898790af12553c53483c8070e7f6172a48f62e9
unknown <kimksy27@gmail.com>
2024-03-23 23:20:11 +0900

    Feat : 회원가입시 환영 모달 후 로그인 페이지 이동

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -9,7 +9,8 @@ import Modal from '@/components/Modal/Modal';
 
 export default function SignUpPage() {
   const router = useRouter();
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isSignupModalOpen, setIsSignupModalOpen] = useState(false);
+  const [isFailSignupModalOpen, setIsFailSignupModalOpen] = useState(false);
 
   const handleSignUpSubmit = async (newUserValues: UserSignUp): Promise<void> => {
     try {
@@ -18,25 +19,27 @@ export default function SignUpPage() {
         return;
       }
       if (res.ok) {
-        alert('가입이 완료되었습니다');
-        router.push('/login');
+        setIsSignupModalOpen(true);
+        setTimeout(() => setIsSignupModalOpen(false), 1000);
+        setTimeout(() => router.push('/login'), 1000);
       } else if (res.status === 409) {
-        setIsModalOpen(true);
+        setIsFailSignupModalOpen(true);
       }
     } catch (error) {
       console.error('회원가입 페이지 실패:', error);
     }
   };
 
-  const closeModal = () => {
-    setIsModalOpen(false);
+  const closeFailSignupModal = () => {
+    setIsFailSignupModalOpen(false);
   };
 
   return (
     <div className="flex flex-col items-center justify-center h-screen w-screen">
       <MainLogo title={'첫 방문을 환영합니다!'} />
       <SignUp onSubmit={handleSignUpSubmit} />
-      {isModalOpen && <Modal isOpen={isModalOpen} onClose={closeModal} title="이메일 중복" children="이미 사용중인 이메일입니다" />}
+      {isSignupModalOpen && <Modal isOpen={isSignupModalOpen} title="회원 가입" children="가입 완료! 환영합니다." />}
+      {isFailSignupModalOpen && <Modal isOpen={isFailSignupModalOpen} onClose={closeFailSignupModal} title="회원 가입" children="이미 사용중인 이메일입니다" />}
     </div>
   );
 }
