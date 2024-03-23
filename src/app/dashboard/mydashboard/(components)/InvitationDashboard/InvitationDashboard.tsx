b5492e9f2648d8ae7c1e559b3b51a341da730a24
Jo hyeonjin <guswlso2@naver.com>
2024-03-23 16:24:05 +0900

    feat: 내대시보드 초대장 없을 때 영역구현

--- src/app/dashboard/mydashboard/(components)/InvitationDashboard/InvitationDashboard.tsx
+++ src/app/dashboard/mydashboard/(components)/InvitationDashboard/InvitationDashboard.tsx
@@ -6,25 +6,24 @@ import { getInvitationList } from '@/api/InvitationApi';
 import SearchForm from './SearchForm';
 import InvitationList from './InvitationList';
 import useIntersectionObserver from '@/components/hooks/useObserver/useIntersectionObserver';
+import Image from 'next/image';
 
 export default function InvitationDashboard() {
   const [processedInvitations, setProcessedInvitations] = useState<Invitation[]>([]);
   const [inputValue, setInputValue] = useState('');
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const cursorIdRef = useRef<number | null>(null);
-  const isClosedRef = useRef<boolean>(false);
   const hasNotNext = processedInvitations.length !== 0 && !cursorIdRef.current;
 
   const handleIntersection = async () => {
-    setLoading(true);
     try {
-      if (hasNotNext || loading) {
+      if (hasNotNext && !loading) {
+        console.log('리턴');
         return;
       }
       const { invitations, cursorId } = await getInvitationList(8, cursorIdRef.current, inputValue);
       cursorIdRef.current = cursorId;
       if (!cursorId) {
-        isClosedRef.current = true;
       }
       setProcessedInvitations(prevInvitations => [...prevInvitations, ...invitations]);
     } catch (error) {
@@ -43,9 +42,6 @@ export default function InvitationDashboard() {
       const { invitations, cursorId } = await getInvitationList(8, null, value);
       setProcessedInvitations(invitations);
       cursorIdRef.current = cursorId;
-      if (!cursorIdRef.current) {
-        isClosedRef.current = true;
-      }
     } catch (error) {
       console.error('Error occurred:', error);
     } finally {
@@ -59,8 +55,15 @@ export default function InvitationDashboard() {
       <div className="mb-5 border border-gray-D9D9D9 rounded-md overflow-hidden">
         <SearchForm onSubmit={handleSearchSubmit} />
       </div>
-      <InvitationList processedInvitations={processedInvitations} setProcessedInvitations={setProcessedInvitations} />
-      <div ref={sentinelRef}></div> {/* Intersection Observer 타겟 */}
+      {processedInvitations.length > 0 || loading ? (
+        <InvitationList processedInvitations={processedInvitations} setProcessedInvitations={setProcessedInvitations} />
+      ) : (
+        <div className="flex justify-center">
+          <Image src="/images/no-invitation.svg" height="100" width="100" alt="초대가 없습니다" />
+        </div>
+      )}
+      <div ref={sentinelRef}></div>
+      {/* Intersection Observer 타겟 */}
       {loading && (
         <div className="flex justify-center h-44 items-center">
           <svg className="animate-spin h-10 w-10 border-4 rounded-full border-t-indigo-500" viewBox="0 0 24 24"></svg>
